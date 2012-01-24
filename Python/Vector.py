9d90e9d0aab065e9a8f4de4f5a0dc028c32be267
David Fillmore <greymoth@me.com>
2012-01-23 23:01:56 -0700

    added scalar multiplication and division

--- Python/Vector.py
+++ Python/Vector.py
@@ -10,37 +10,73 @@ class Vector :
    def __getitem__(self, k) :
       return self.comps[k]
 
+   def __setitem__(self, k, a) :
+      self.comps[k] = a
+
    def show(self) :
       print self.comps
 
    # overload arithmetic operators
+
+   # vector addition
    def __add__(self, V) :
       sum = []
       for k in range(self.dim) :
          sum.append(self.comps[k] + V[k])
       return Vector(sum)
 
+   # vector subtraction
    def __sub__(self, V) :
       diff = []
       for k in range(self.dim) :
          diff.append(self.comps[k] - V[k])
       return Vector(diff)
 
-def testAssignmentOperator() :
-   V = Vector([0.0, 0.0, 0.0])
+   # vector negation
+   def __neg__(self) :
+      minus = []
+      for k in range(self.dim) :
+         minus.append(- self.comps[k])
+      return Vector(minus)
+
+   # scalar multiplication
+   def __mul__(self, a) :
+      prod = []
+      for k in range(self.dim) :
+         prod.append(self.comps[k] * a)
+      return Vector(prod)
+
+   # scalar division
+   def __div__(self, a) :
+      quot = []
+      for k in range(self.dim) :
+         quot.append(self.comps[k] / a)
+      return Vector(quot)
+
+def testAssignmentOperators() :
+   V = Vector([1.0, 2.0, 3.0])
    print V.size()
    V.show()
+   W = V
+   W.show()
    for k in range(V.size()) :
       print V[k]
+   for k in range(V.size()) :
+      V[k] = 0.0
+   V.show()
 
 def testArithmeticOperators() :
    V = Vector([0.0, 1.0, 2.0])
    W = Vector([3.0, 4.0, 5.0])
-   sum = V + W
-   sum.show()
-   diff = V - W
-   diff.show()
+   U = V + W
+   U.show()
+   U = V - W
+   U.show()
+   U = V * 2.0
+   U.show()
+   U = V / 2.0
+   U.show()
 
 if __name__ == '__main__' :
-   testAssignmentOperator()
+   testAssignmentOperators()
    testArithmeticOperators()
