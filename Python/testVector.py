9efc4e7653a82292fc48c4d56d694daf9f3c198a
David Fillmore <greymoth@me.com>
2012-01-23 23:08:05 -0700

    created seperate unit test file

---
+++ Python/testVector.py
@@ -0,0 +1,30 @@
+from Vector import *
+
+def testAssignmentOperators() :
+   V = Vector([1.0, 2.0, 3.0])
+   print V.size()
+   V.show()
+   W = V
+   W.show()
+   for k in range(V.size()) :
+      print V[k]
+   for k in range(V.size()) :
+      V[k] = 0.0
+   V.show()
+
+def testArithmeticOperators() :
+   V = Vector([0.0, 1.0, 2.0])
+   W = Vector([3.0, 4.0, 5.0])
+   U = V + W
+   U.show()
+   U = V - W
+   U.show()
+   U = V * 2.0
+   U.show()
+   U = V / 2.0
+   U.show()
+
+if __name__ == '__main__' :
+   testAssignmentOperators()
+   testArithmeticOperators()
+
